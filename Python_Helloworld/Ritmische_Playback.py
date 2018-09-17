9697af7f93760f25a9f3d3750885467639511f27
WesBroersen <Wes@MacBook-Pro-van-Wes.local>
2018-09-17 18:53:56 +0200

    Final Version V1.0

--- Python_Helloworld/Ritmische_Playback.py
+++ Python_Helloworld/Ritmische_Playback.py
@@ -1,42 +1,45 @@
 RhytmList = []
+y = 0
 
 #Import de audio sample
 import simpleaudio as sa
 from time import sleep
 
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/wesbroersen 1/Documents/Samples/rhythm-lab.com_amen_vol.1/WAV/cw_amen01_175.wav")
+wave_obj = sa.WaveObject.from_wave_file("/Users/wesbroersen 1/Documents/Samples/wijnglas samples/glas water a hard.wav")
 
 #Define the number of playback times
-print("numPlaybackTimes:")
+print("numPlaybackTimes: ___________________________")
 numPlaybackTimes = input()
 #Convert the variable into a integer
 numPlaybackTimes = int(numPlaybackTimes)
 
 #Fill a list defined by Playback times with numbers in RhytmList
-print("Rhytm:")
+print("Rhytm: _________________________")
 for x in range(0, numPlaybackTimes):
     Rhytm = input()
     float(Rhytm)
     RhytmList.append(Rhytm)
 
+print(RhytmList)
+
 #Define the tempo in the variable called BPM
-print("BPM:")
+print("BPM: ___________________________")
 BPM = input()
 #Convert BPM to floating point value
 x = int(BPM)
 #Get the BPM in Seconds to work in the delay
 BPMMs = 60 / x
 
-print(BPMMs)
 
-
-
-number = 0
+play_obj = wave_obj.play()
+play_obj.wait_done()
 
 for x in range(0, numPlaybackTimes):
 
+
+    print(y)
+    sleep(BPMMs * float((RhytmList[y])))
     play_obj = wave_obj.play()
     play_obj.wait_done()
-
-    sleep(RhytmList[# hier moet het getal uit the Rhytmlist komen die bepaald hoeland de sleep functie moet werken ])
+    y = y + 1
